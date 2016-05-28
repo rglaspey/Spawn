aba364b66413fd16a6ba13cd1f003ff2288b0b69
Patrick Glaspey <pglaspey@gmail.com>
2016-05-28 08:51:06 -0700

    Added CreateSpecTree to SpawnSpec interface.

--- src/SixtenLabs.Spawn/Interfaces/ISpawnSpec.cs
+++ src/SixtenLabs.Spawn/Interfaces/ISpawnSpec.cs
@@ -1,8 +1,12 @@
-﻿namespace SixtenLabs.Spawn
+﻿using AutoMapper;
+
+namespace SixtenLabs.Spawn
 {
 	public interface ISpawnSpec<T> where T : class
 	{
 		void ProcessRegistry();
+
+		void CreateSpecTree(IMapper mapper);
 
 		T SpecTree { get; }
 
